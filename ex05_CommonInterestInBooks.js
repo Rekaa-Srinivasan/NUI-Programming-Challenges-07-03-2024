09151714e8e2b38ed888734de054994b499d0ca1
Rekaa-Srinivasan <162275345+Rekaa-Srinivasan@users.noreply.github.com>
2024-03-07 16:49:50 +0530

    Updated ex05_CommonInterestInBooks.js

--- ex05_CommonInterestInBooks.js
+++ ex05_CommonInterestInBooks.js
@@ -11,7 +11,6 @@ function findCommonInterestInBooks(students){
             uniqueBookList.add(book);
         }
     }
-    // console.log(uniqueBookList.values());
     for(const uniqueBook of uniqueBookList.values()){
         const groupingCommonInterest = [];
         outerloop: for(let studentId = 0; studentId < students.length; studentId++){
@@ -25,28 +24,5 @@ function findCommonInterestInBooks(students){
     }
     
 }
-
-console.log(findCommonInterestInBooks([
-    {
-      id: 0,
-      name: 'Arun',
-      books: ['Wings of Fire', 'Chakra'],
-    },
-    {
-      id: 1,
-      name: 'Ashok',
-      books: ['Chakra', 'War and Peace', 'The Shining']
-    },
-    {
-      id: 2,
-      name: 'Balu',
-      books: ['Wings of Fire', 'All about Cricket'],
-    },
-    {
-      id: 3,
-      name: 'Cathi',
-      books: ['Against the wind', 'The Shining', 'War and Peace']
-    },
-]))
 
 
